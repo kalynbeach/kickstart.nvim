f6614bd76633476308efd5f37bd5da2bcdd9421e
kalynbeach <kalynbeach@gmail.com>
2024-07-25 13:18:46 -0700

    update init.lua

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -786,6 +786,16 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
+
+      local lackluster = require 'lackluster'
+
+      lackluster.setup {
+        tweak_syntax = {
+          comment = lackluster.color.gray4,
+        },
+        disable_plugin = {},
+      }
+
       vim.cmd.colorscheme 'lackluster-hack'
 
       -- You can configure highlights by doing something like:
